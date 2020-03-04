698d7c58980e36a5a53d0cf858621cd4bbd487f1
unknown <SilviaDumitrache>
2020-03-04 18:05:27 +0200

    project setup

---
+++ Lab-1/src/ro/unitbv/javadatatype/Exercitiu1.java
@@ -0,0 +1,12 @@
+package ro.unitbv.javadatatype;
+
+public class Exercitiu1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+
+	}
+
+}
